a80a10778d9be63758a10c4244e56c06232e818b
James Chuang <chuang.ja@gmail.com>
2019-06-18 18:22:03 -0400

    Tweaks for fullscreen images.

--- compile.sh
+++ compile.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 module load texlive
-# latexmk -pdfxe -f thesis.tex
-# latexmk -c
+latexmk -pdfxe -f thesis.tex
+latexmk -c
 latexmk -pdfxe -f presentation.tex
 latexmk -c
+# latexmk -pdfxe -f acknowledgements.tex
+# latexmk -c
